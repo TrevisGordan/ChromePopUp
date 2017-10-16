7f385a511a0d909736d7dfd9a5e45abc8178286a
Trevis Gordan <fenster.gewinn@yahoo.de>
2017-10-16 13:48:56 +0200

    getword fixed

--- js/background.js
+++ js/background.js
@@ -6,7 +6,7 @@ chrome.browserAction.onClicked.addListener( function(tab) {
 
 });
 
-function normalwindow(info,tab) {
+function getword(info,tab) {
   console.log("Word " + info.selectionText + " was clicked.");
   chrome.tabs.create({
     url: "http://www.google.com/search?q=" + info.selectionText,

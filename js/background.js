cb03d5f241277b0f30d4f0c7fd7b0f7abe2ad341
Trevis Gordan <fenster.gewinn@yahoo.de>
2017-10-16 13:57:49 +0200

    Added windowChange

--- js/background.js
+++ js/background.js
@@ -6,6 +6,17 @@ chrome.browserAction.onClicked.addListener( function(tab) {
 
 });
 
+
+function windowChange() {
+	chrome.windows.create({ tabId: 	tab.id,
+							type: 	"normal" });
+}
+chrome.contextMenus.create({
+  title: "Change Window",
+  onclick: windowChange,
+});
+
+
 function getword(info,tab) {
   console.log("Word " + info.selectionText + " was clicked.");
   chrome.tabs.create({

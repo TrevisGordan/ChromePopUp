bbe451d20e518239c7ded819fb32ccdc2452ea23
Trevis Gordan <fenster.gewinn@yahoo.de>
2017-10-16 13:43:25 +0200

    Just Added REndom context menu

--- js/background.js
+++ js/background.js
@@ -5,3 +5,15 @@ chrome.browserAction.onClicked.addListener( function(tab) {
 							type: 	"popup" });
 
 });
+
+function normalwindow(info,tab) {
+  console.log("Word " + info.selectionText + " was clicked.");
+  chrome.tabs.create({
+    url: "http://www.google.com/search?q=" + info.selectionText,
+  });
+}
+chrome.contextMenus.create({
+  title: "Search: %s",
+  contexts:["selection"],
+  onclick: getword,
+});
